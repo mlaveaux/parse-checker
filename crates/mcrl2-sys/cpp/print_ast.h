5faa8cdee8fae15e533f10316857db98bc4e609a
Maurice Laveaux <m.laveaux@tue.nl>
2025-07-01 14:11:45 +0200

    Call the parse function that does not require an LPS.

--- crates/mcrl2-sys/cpp/print_ast.h
+++ crates/mcrl2-sys/cpp/print_ast.h
@@ -20,15 +20,7 @@ rust::String print_ast_mcrl2(rust::Str text) {
 
 inline
 rust::String print_ast_mcf(rust::Str text) {
-    mcrl2::lps::stochastic_specification lpsspec;
-
-    mcrl2::state_formulas::parse_state_formula_options options;
-    options.type_check = false;
-    options.translate_regular_formulas = false;
-    options.translate_user_notation = false;
-    options.resolve_name_clashes = false;
-    options.check_monotonicity = false;
-    mcrl2::state_formulas::state_formula_specification formspec = mcrl2::state_formulas::parse_state_formula_specification(static_cast<std::string>(text), lpsspec, false, options);
+    mcrl2::state_formulas::state_formula_specification formspec = mcrl2::state_formulas::detail::parse_state_formula_specification(static_cast<std::string>(text));
 
     std::stringstream result;
     result << mcrl2::state_formulas::pp(formspec, false) << std::endl;
